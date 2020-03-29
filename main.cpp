11ce1dda0fcaa5565121c2db9d672fe019643520
Hanssen <35262249+hanssenstijn@users.noreply.github.com>
2020-03-29 17:49:13 +0200

    basic commands/script C++
    using <iostream>
    
    using std::cout -- package
    
    define the functions as:
    
    int main() 
    {
    define a variable
    assing value to variable
    print out text + variable using : cout
    }

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// using namespace std; // using directive
+using std::cout; // define specific object from package
+
+
+int main() // main function
+{
+  // std::cout << "Hello World\n"; // standard way
+  int slices; // declaration & initialization
+  slices = 1;
+  cout << slices << "\nHello World\n"; 
+  // cout << "\nHello World\n";
+
+  // printf("%i\n",slices); // different way of printing
+}
+
